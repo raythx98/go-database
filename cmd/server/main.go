590e6ccd7cfafbb42e148155d33f5b959151268f
Ray Toh <raythx98@gmail.com>
2023-02-07 18:57:04 +0800

    Refactor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"github.com/raythx98/go-database/service"
+	"fmt"
+	pb "github.com/raythx98/go-database/pb"
+	"github.com/raythx98/go-database/service/handler"
+	"google.golang.org/grpc"
+	"log"
+	"net"
 )
 
 func main() {
-	service.StartService()
+	db, err := Init()
+	defer db.Close()
+	if err != nil {
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+
+	log.Println("Listening...")
+
+	// can listen from any IP Address
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", 9282))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+
+	pb.RegisterDatabaseServiceServer(grpcServer, &handler.Server{})
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatalf("failed to listen to gRPC port 9282: %v", err)
+	}
 }
